0c91522b8b7c7536f2d0c2032c57613bfcfb36c8
machzqcq <pradeep@seleniumframework.com>
2015-01-11 01:00:17 -0500

    Fire GRID using json config

---
+++ START_HUB_USING_JSON.bat
@@ -0,0 +1,8 @@
+set HERE=%CD%
+set JAVA_HOME=%JAVA_HOME%
+set PATH=%JAVA_HOME%\jre\bin;%JAVA_HOME%\bin;%PATH%
+set SELENIUM_VERSION=2.44.0
+set HOST=localhost
+set PORT=4444
+
+start java -jar selenium-server-standalone-%SELENIUM_VERSION%.jar -role hub -hubConfig hubConfig.json -log SELENIUMLOGS\hub.log
---
+++ START_NODE_JSON_CH39_FF35_IE10_WINDOWS.bat
@@ -0,0 +1,26 @@
+set HERE=%CD%
+set JAVA_HOME=%JAVA_HOME%
+set PATH=%JAVA_HOME%\jre\bin;%JAVA_HOME%\bin;%PATH%
+set SELENIUM_VERSION=2.44.0
+set PLATFORM=WINDOWS
+
+set CHROME_VERSION=39
+set CHROME_WEBDRIVER_VERSION=2.13
+set CHROME_WEBDRIVER_LOC=%HERE%/CHROMEDRIVERS/%CHROME_WEBDRIVER_VERSION%/chromedriver.exe
+set CHROME_BINARY_LOC=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
+
+set FIREFOX_VERSION=35
+set FIREFOX_BINARY_LOC=C:\Program Files (x86)\Mozilla Firefox\firefox.exe
+
+set IE_VERSION=10
+set IE_WEBDRIVER_VERSION=2.44
+set IE_WEBDRIVER_LOC=%HERE%/IEDRIVERS/%IE_WEBDRIVER_VERSION%/IEDriverServer.exe
+
+set HUB_HOST=localhost
+set HUB_PORT=4444
+set HUB_URL=http://%HUB_HOST%:%HUB_PORT%/grid/register
+
+set NODE_HOST=localhost
+set NODE_PORT=5558
+
+start java -jar selenium-server-standalone-%SELENIUM_VERSION%.jar -role node -Dwebdriver.chrome.driver=%CHROME_WEBDRIVER_LOC% -Dwebdriver.ie.driver=%IE_WEBDRIVER_LOC% -log SELENIUMLOGS\selnode-%NODE_PORT%.log -nodeConfig nodeConfig.json
